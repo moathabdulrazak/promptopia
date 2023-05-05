cad258df0eecee49c91cb1880e98f0ebf5c2be3b
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-05 16:46:31 -0600

    ss

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,16 @@
+'use client'
+import Link from 'next/link'
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3" >
+        <Link className="flex gap-2 flex-center"  href={'/'} >
+          <Image src="/assets/images/logo.svg" />
+        </Link>
+    </nav>
   )
 }
 
