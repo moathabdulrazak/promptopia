c41926036b29c6db0569c1613499e5d9d4b51a3c
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-05 16:50:41 -0600

    s

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,7 +8,11 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3" >
         <Link className="flex gap-2 flex-center"  href={'/'} >
-          <Image src="/assets/images/logo.svg" />
+          <Image src="/assets/images/logo.svg"
+          alt="Promptopia logo" 
+          width={40}
+          height={40}
+          className="object-contain"/>
         </Link>
     </nav>
   )
