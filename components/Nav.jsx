c859e32aa4fdcb8e11a20ed29f25415f0d05714d
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-05 17:00:08 -0600

    s

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react"
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
+
+  const isUserLoggedIn = true
   return (
     <nav className="flex-between w-full mb-16 pt-3" >
         <Link className="flex gap-2 flex-center"  href={'/'} >
@@ -13,7 +15,23 @@ const Nav = () => {
           width={40}
           height={40}
           className="object-contain"/>
+          <p className="logo_text" > Promptopia</p>
         </Link>
+
+        {/* mobile navigation */}
+        <div className="sm:flex hidden" >
+          {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5" >
+            <Link className="black_btn" href={'/create-prompt'} >
+            Create Post
+            </Link>
+          </div>
+          ) : (
+            <>
+            
+            </>
+          ) }
+        </div>
     </nav>
   )
 }
