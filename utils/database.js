a89ecaf487a1d1968995bf331dec0374801c3d42
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-06 00:59:59 -0600

    next fixes.

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,28 @@
-import mongoose from 'mongoose';
 
-let isConnected = false; 
 
-export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+const mongoose = require('mongoose');
 
-  if(isConnected) {
-    console.log('MongoDB is already connected');
+let isConnected = false;
+
+const connectToDb = async () => {
+  if (isConnected) {
+    console.log('Already connected to MongoDB');
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB, {
-      dbName: "promptopia_ai",
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
 
     isConnected = true;
-
-    console.log('MongoDB connected')
+    console.log('Connected to MongoDB');
   } catch (error) {
-    console.log(error);
+    console.error('Failed to connect to MongoDB:', error.message);
   }
-}
+};
+
+module.exports = connectToDb;
